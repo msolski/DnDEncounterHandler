76d7f34d3a28d95cc24511cc13d5cf1eef6c7f56
msolski <michaelavsolski@rogers.com>
2017-06-07 19:54:37 -0400

    Barely did anything

--- Encounter Handler/src/handler/MainView.java
+++ Encounter Handler/src/handler/MainView.java
@@ -2,6 +2,8 @@ package handler;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
@@ -9,10 +11,15 @@ import javax.swing.event.MenuListener;
 //Main window, container of all panels
 public class MainView {
 
+	public static final short WIDTH = 650; //Width of window 
+	public static final short HEIGHT = 150; //Height of each panel
+	private static int amtChars = 0; //Number of characters
+	
 	JFrame frame;
 	
 	public static void main(String[] args) {
 		MainView window = new MainView();
+		window.frame.setSize(WIDTH, HEIGHT);
 	}
 	
 	public MainView(){
@@ -20,12 +27,7 @@ public class MainView {
 		frame = new JFrame("Michael's too lazy to use a pencil and paper simulator v0.1");
 		frame.setLayout(new BorderLayout());
 		
-		//Filler panel
-		JPanel filler = new JPanel();
-		filler.setOpaque(true);
-		filler.setBackground(new Color(250,250,253));
-		
-		//Menu bar
+		//Menu bar - TODO: Maybe make these not on a menu? Kinda finicky
 		JMenuBar menubar = new JMenuBar();
 		JMenu newPlayerMNU = new JMenu("New Player");
 		
@@ -33,8 +35,14 @@ public class MainView {
 		newPlayerMNU.addMenuListener(new MenuListener() {
 		    @Override
 		    public void menuSelected(MenuEvent e) {
+		    	amtChars++;
 		    	String playerName = JOptionPane.showInputDialog("Player name");
+		    	frame.setSize(WIDTH, HEIGHT*amtChars);
 				frame.add(new PlayerPanel(playerName));
+				frame.revalidate();
+				frame.validate();
+				frame.setVisible(true);
+				System.out.println(frame.getHeight());
 		    }
 		    //Unused
 		    @Override
@@ -50,7 +58,6 @@ public class MainView {
 		
 		//Add everything to the frame
 		frame.setJMenuBar(menubar);
-		frame.add(filler);
 		frame.setVisible(true);
 	}
 }
--- Encounter Handler/src/handler/PlayerPanel.java
+++ Encounter Handler/src/handler/PlayerPanel.java
@@ -1,8 +1,10 @@
 package handler;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 /*
 Player information panel, to be added onto the window.
@@ -10,12 +12,22 @@ Not a lot of info, because the players are supposed to keep track of that.
 The DM needs some mystery too.*/
 
 public class PlayerPanel extends JPanel {
-	JPanel panel;
+
+	private static final long serialVersionUID = 1L;
 	
 	public PlayerPanel(String name){
 		super();
+		this.setSize(WIDTH, HEIGHT-20);
+		this.setLayout(new BorderLayout());
 		this.setOpaque(true);
-		this.setBackground(new Color(250,250,253));
+		this.setBackground(new Color(175, 175, 175));
+		this.setVisible(true);
+		this.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		JTextArea nameField = new JTextArea(name);
+		
+		this.add(nameField, BorderLayout.WEST);
+		
 		System.out.println(name);
 	}
 }
